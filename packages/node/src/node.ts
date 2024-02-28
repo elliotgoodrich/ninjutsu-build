d07fd7e10407678e2fbceb0ec426f4080508281e
Elliot Goodrich <elliotgoodrich@gmail.com>
2024-02-28 17:28:06 +0000

    Fix `@ninjutsu-build/node` args variable
    The `args` variable really should be used for command line arguments
    passed to the JavaScript file rather than command line arguments for
    node or v8.  node and v8 flags can now be passed with `nodeArgs`.

--- packages/node/src/node.ts
+++ packages/node/src/node.ts
@@ -22,8 +22,8 @@ const importCode =
 // we mention `node.exe` with the file extension to avoid the `winpty node` alias.
 const node = platform() === "win32" ? "cmd /c node.exe" : "node";
 
-const command = `${node} --require "${hookRequire}" --import "data:text/javascript,${importCode}" $args $in > $out`;
-const testCommand = `${node} --require "${hookRequire}" --import "data:text/javascript,${importCode}" --test --test-reporter=${testReporter} --test-reporter=tap --test-reporter-destination=stderr --test-reporter-destination=$out $in`;
+const command = `${node} --require "${hookRequire}" --import "data:text/javascript,${importCode}" $nodeArgs $in -- $args > $out`;
+const testCommand = `${node} --require "${hookRequire}" --import "data:text/javascript,${importCode}" --test --test-reporter=${testReporter} --test-reporter=tap --test-reporter-destination=stderr --test-reporter-destination=$out $nodeArgs $in -- $args`;
 
 /**
  * Create a rule in the specified `ninja` builder with the specified `name` that will
@@ -61,6 +61,7 @@ export function makeNodeRule(
   in: Input<string>;
   out: O;
   args?: string;
+  nodeArgs?: string;
   [implicitDeps]?: string | readonly string[];
   [orderOnlyDeps]?: string | readonly string[];
   [implicitOut]?: string | readonly string[];
@@ -74,6 +75,7 @@ export function makeNodeRule(
     depfile: "$out.depfile",
     deps: "gcc",
     args: "",
+    nodeArgs: "",
   });
 }
 
@@ -114,6 +116,7 @@ export function makeNodeTestRule(
   in: Input<string>;
   out: O;
   args?: string;
+  nodeArgs?: string;
   [implicitDeps]?: string | readonly string[];
   [orderOnlyDeps]?: string | readonly string[];
   [implicitOut]?: string | readonly string[];
@@ -127,5 +130,6 @@ export function makeNodeTestRule(
     depfile: "$out.depfile",
     deps: "gcc",
     args: "",
+    nodeArgs: "",
   });
 }
