ac697ff5ed3780ddb39f750886eda4d4fb19327e
Elliot Goodrich <elliotgoodrich@gmail.com>
2026-05-27 08:09:15 +0100

    Fix @ninjutsu-build/node require recursion
    The injected require in `hookRequire.ts` is being called for itself.
    This is failing to resolve and throwing an exception, causing NodeJS
    to fail to run any JavaScript run with this hook.
    
    We catch exceptions thrown here to silence the error (perhaps this is
    not the best fix), and this also will delegate throwing exceptions in
    the case we do not find a module to the default `require` instead of
    our `require.resolve` call.

--- packages/node/src/hookRequire.ts
+++ packages/node/src/hookRequire.ts
@@ -7,7 +7,16 @@ Module.prototype.require = function (this: NodeModule, id: string): string {
   const paths = require.resolve.paths(id);
   // If `paths` is null then this is a core module (e.g. http or fs)
   if (paths !== null) {
-    addDependency(require.resolve(id, { paths: [this.path] }));
+    try {
+      addDependency(require.resolve(id, { paths: [this.path] }));
+    } catch (e) {
+      // If the module cannot be found then swallow the exception and
+      // delegate to default behavior when calling `require` below.
+      // Note that our `require` function is also called for
+      // `hookRequire.js` as of Node v24 and `require.resolve` throws
+      // an exception when trying to resolve `./hookRequire.js` from
+      // itself.
+    }
   }
   return r.call(this, id);
 };
